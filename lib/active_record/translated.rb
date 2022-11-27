cd38f469724b1d6ea3b3ee77a9a468e89c0ba27f
Rasmus <2277443+kjellberg@users.noreply.github.com>
2022-11-27 23:24:31 +0100

    feat: finder methods

--- lib/active_record/translated.rb
+++ lib/active_record/translated.rb
@@ -18,7 +18,13 @@ module ActiveRecord
 
     class << self
       def generate_record_id
-        SecureRandom.uuid
+        SecureRandom.uuid.split("-")[1..3].join("-")
+      end
+
+      def record_id?(string)
+        return false unless string.is_a?(String)
+
+        string.match(/^[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}$\z/)
       end
 
       def locale
--- lib/active_record/translated/model.rb
+++ lib/active_record/translated/model.rb
@@ -6,25 +6,48 @@ module ActiveRecord
     module Model
       extend ActiveSupport::Concern
 
+      # rubocop:disable Metrics/BlockLength
       included do
         before_save :set_record_id
         before_save :set_locale
 
-        has_many :translations, class_name: "Post", foreign_key: :record_id, primary_key: :record_id
-        scope :translated, -> { where(locale: ActiveRecord::Translated.locale) }
+        has_many :translations, -> { global }, class_name: "Post", foreign_key: :record_id, primary_key: :record_id
+        default_scope { where(locale: ActiveRecord::Translated.locale) }
+        scope :global, -> { unscope(where: :locale) }
 
         class << self
-          def find_translated(record_id)
-            translated.find_by(record_id: record_id)
+          def find(*args)
+            id = args.first
+
+            return find_translated(id) if ActiveRecord::Translated.record_id?(id)
+
+            super(*args)
           end
 
-          def find_translated!(record_id)
-            translated.find_by!(record_id: record_id)
+          def exists?(id)
+            return super unless ActiveRecord::Translated.record_id?(id)
+            return true if exists_translated?(id)
+
+            super
           end
+
+          private
+
+          def find_translated(record_id, allow_nil: false)
+            result = find_by(record_id: record_id)
+            return result if result
+
+            raise ActiveRecord::RecordNotFound unless allow_nil
+          end
+
+          # rubocop:disable Rails/WhereExists
+          def exists_translated?(record_id)
+            where(record_id: record_id, locale: ActiveRecord::Translated.locale).exists?
+          end
+          # rubocop:enable Rails/WhereExists
         end
       end
-
-      private
+      # rubocop:enable Metrics/BlockLength
 
       def set_locale
         return if locale.present?
